be1609f3ede1010bc68b89a9cda1cb6a2ebdf9c7
Mihretu <mihretugonche@gmail.com>
2026-02-12 12:09:03 +0300

    update server

--- server.py
+++ server.py
@@ -3,7 +3,8 @@ from quotes import get_sad_quote, get_happy_quote
 
 mcp = FastMCP("AXBV-qoutes-server")  # MCP server name
 @mcp.tool(
-    name = "Get-AXBV-happy-quote"
+    name="Get-AXBV-happy-quote",
+    description="Return a random happy quote from the AXBV quotes collection."
 )
 def get_axbv_happy_quote() -> str:
     return get_happy_quote()
